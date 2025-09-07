0e99507a632fb91c7f5e3907db6d71722453a285
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-09-06 23:37:35 -0300

    feat: add is authentiicated middlewares to verify only signIn user can be created new users

---
+++ src/middlewares/auth/isAuthenticated.ts
@@ -0,0 +1,30 @@
+import type { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+interface IsAuthenticatedProps {
+  sub: string;
+}
+
+export function IsAuthenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authToken = req.headers.authorization;
+
+  if (!authToken) {
+    res.status(400).send({ message: "Credenciais invalidas!" }).end();
+  }
+
+  const [, token] = authToken?.split(" ");
+
+  try {
+    const { sub } = jwt.verify(
+      token as string,
+      process.env.JWT_SECRET!
+    ) as IsAuthenticatedProps;
+    return next();
+  } catch (err) {
+    res.status(400).send({ message: "Authorization error!" }).end();
+  }
+}
