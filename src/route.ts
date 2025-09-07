6a9d1805e973b76b13f387ee974b4b6a7c88b202
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-09-06 23:28:07 -0300

    feat: add router to register user

--- src/route.ts
+++ src/route.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { CreateUserController } from "./controllers/user/createUserController.ts";
 import { GetUserController } from "./controllers/user/getUserController.ts";
+import { AuthUserController } from "./controllers/user/authUserController.ts";
 
 const router = Router();
 
 router.get("/users", new GetUserController().handle);
-router.post("/session", new CreateUserController().handle);
+router.post("/users", new CreateUserController().handle);
+router.post("/session", new AuthUserController().handle);
 
 export { router };
