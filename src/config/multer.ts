a41d4fa0527af48a083e952650705ad02a3bf7b7
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-09-09 17:58:08 -0300

    fix: importing dirname as meta because we have the "type" as 'module'

--- src/config/multer.ts
+++ src/config/multer.ts
@@ -3,17 +3,14 @@ import multer from "multer";
 import { resolve, extname } from "path";
 
 export default {
-  upload(folder: string /* pasta em que deseja salvar*/) {
+  upload(folder: string) {
     return {
       storage: multer.diskStorage({
-        destination: resolve(__dirname, "..", "..", folder),
-        filename(req, file, callback) {
-          const fileHash = crypto.randomBytes(15).toString("hex");
+        destination: resolve(import.meta.dirname, "..", "..", folder),
+        filename: (request, file, callback) => {
+          const fileHash = crypto.randomBytes(16).toString("hex");
           const fileName = `${fileHash}-${file.originalname}`;
-          return callback(
-            /* Primeiro error (nao vamos tratar)*/ null,
-            fileName
-          );
+          return callback(null, fileName);
         },
       }),
     };
