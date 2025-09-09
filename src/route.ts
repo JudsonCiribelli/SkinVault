d093944802253d8b98922bbfe553aa3f8df511fb
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-09-09 17:24:50 -0300

    refactor: update routes archive

--- src/route.ts
+++ src/route.ts
@@ -13,6 +13,6 @@ router.get("/users", new GetUserController().handle);
 router.get("/user/:id", IsAuthenticated, new GetUserByIdController().handle);
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
-router.get("/category", new CreateCategoryController().handle);
+router.post("/category", new CreateCategoryController().handle);
 
 export { router };
