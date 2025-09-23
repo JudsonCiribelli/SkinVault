160f820b563582938c0b7f0079e146c7a1163f53
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-09-23 17:53:25 -0300

    fix: get user id logged

--- src/controllers/order/createPurchaseController.ts
+++ src/controllers/order/createPurchaseController.ts
@@ -1,17 +1,28 @@
 import type { Request, Response } from "express";
 import { CreatePurchaseService } from "../../services/order/createPurchaseService.ts";
+import { CreateCheckoutService } from "../../services/payment/createCheckoutProService.ts";
 
 class CreatePurchaseController {
   async handle(req: Request, res: Response) {
-    const { sellingItemId, buyerId } = req.body;
-    const createPurchaseService = new CreatePurchaseService();
+    const { sellingItemId } = req.body;
+    const buyerId = req.userId;
 
-    const orderPurchase = await createPurchaseService.execute({
-      sellingItemId,
-      buyerId,
-    });
+    try {
+      const createCheckoutService = new CreateCheckoutService();
+      const createPurchaseService = new CreatePurchaseService(
+        createCheckoutService
+      );
 
-    return res.status(200).send({ orderPurchase });
+      const orderPurchase = await createPurchaseService.execute({
+        sellingItemId,
+        buyerId,
+      });
+      return res.status(200).send({ orderPurchase });
+    } catch (error) {
+      console.error("Erro no CreatePurchaseController:", error);
+      // Retorna a mensagem de erro do serviço para o cliente
+      return res.status(400).json({ error });
+    }
   }
 }
 export { CreatePurchaseController };
