43b8a91d6728d4b88c7559470c6d21b3dc7e1991
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-10-13 16:13:00 -0300

    test: refactor for async server and implementation setup/teardown hooks and ensure all error routes have been tested

--- tests/categoryItem/categoryItem.test.ts
+++ tests/categoryItem/categoryItem.test.ts
@@ -1,13 +1,31 @@
 import supertest from "supertest";
-import { expect, test } from "vitest";
-import { server } from "../../src/server.ts";
+import { afterAll, beforeAll, expect, test } from "vitest";
+import { startServer } from "../../src/server.ts";
 import { faker } from "@faker-js/faker";
 import { makeCategory } from "../factories/makeCategory.ts";
+import type { Express } from "express";
+import prismaClient from "../../src/lib/client.ts";
+import { makeAuthenticatedUser } from "../factories/makeUser.ts";
+
+let server: Express;
+
+beforeAll(async () => {
+  process.env.NODE_ENV = "test";
+  server = await startServer();
+});
+
+afterAll(async () => {
+  await prismaClient.$disconnect();
+});
 
 test("Create a new Category item", async () => {
   const { result } = await makeCategory();
+
+  const { token } = await makeAuthenticatedUser();
+
   const response = await supertest(server)
     .post("/category/categoryItem")
+    .set("Authorization", `Bearer ${token}`)
     .set("Content-Type", "application/json")
     .send({
       name: faker.lorem.words(4),
@@ -15,4 +33,36 @@ test("Create a new Category item", async () => {
     });
 
   expect(response.status).toBe(200);
+  expect(response.body).toEqual({
+    name: expect.any(String),
+    categoryId: expect.any(String),
+    id: expect.any(String),
+    createdAt: expect.any(String),
+    updatedAt: expect.any(String),
+  });
+});
+
+test("Category id doest not exists", async () => {
+  const { result } = await makeCategory();
+
+  const { token } = await makeAuthenticatedUser();
+
+  const id = await prismaClient.category.findUnique({
+    where: {
+      id: result.id,
+    },
+  });
+
+  const idDoesNotExist = id!;
+
+  const response = await supertest(server)
+    .post("/category/categoryItem")
+    .set("Authorization", `Bearer ${token}`)
+    .set("Content-Type", "application/json")
+    .send({
+      name: faker.lorem.words(4),
+      id: idDoesNotExist,
+    });
+
+  expect(response.status).toBe(400);
 });
