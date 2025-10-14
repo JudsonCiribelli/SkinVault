c320c58e5a3f5deae9b5187c68818dfabaa79267
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-10-14 11:18:33 -0300

    fix: removing unnecessary user creation

--- tests/factories/makeCategoryNameSkin.ts
+++ tests/factories/makeCategoryNameSkin.ts
@@ -1,11 +1,10 @@
 import { faker } from "@faker-js/faker";
 import prismaClient from "../../src/lib/client.ts";
 import { makeCategoryItem } from "./makeCategory.ts";
-import { makeUser } from "./makeUser.ts";
 
 export const makeCategoryNameSkin = async () => {
-  const user = await makeUser();
   const categoryItem = await makeCategoryItem();
+
   const categoryNameSkin = await prismaClient.categoryNameSkin.create({
     data: {
       name: faker.lorem.words(3).toString(),
@@ -13,9 +12,9 @@ export const makeCategoryNameSkin = async () => {
       float: faker.number.float(),
       price: faker.lorem.words(10),
       wear: faker.lorem.words(2),
-      imageUrl: faker.lorem.words(4),
+      imageUrl: faker.image.url(),
       sellerName: faker.person.fullName(),
-      ownerId: user.user.id,
+      ownerId: faker.string.uuid(),
     },
   });
   return { categoryNameSkin };
