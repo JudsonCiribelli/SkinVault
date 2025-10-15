fdb7636986da93499e47c636d78883233b8e60b3
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-10-15 15:37:23 -0300

    fix: add user and category item to factorie to create a new category name skin

--- tests/factories/makeCategoryNameSkin.ts
+++ tests/factories/makeCategoryNameSkin.ts
@@ -1,21 +1,24 @@
 import { faker } from "@faker-js/faker";
 import prismaClient from "../../src/lib/client.ts";
+import { makeUser } from "./makeUser.ts";
 import { makeCategoryItem } from "./makeCategory.ts";
 
 export const makeCategoryNameSkin = async () => {
-  const categoryItem = await makeCategoryItem();
+  const { categoryItem } = await makeCategoryItem();
+  const { user } = await makeUser();
 
   const categoryNameSkin = await prismaClient.categoryNameSkin.create({
     data: {
-      name: faker.lorem.words(3).toString(),
-      categoryItemId: categoryItem.categoryItem.id,
-      float: faker.number.float(),
-      price: faker.lorem.words(10),
+      name: faker.lorem.words(3),
+      categoryItemId: categoryItem.id,
+      ownerId: user.id,
+      float: faker.number.float().toString(),
+      price: faker.finance.amount(),
       wear: faker.lorem.words(2),
       imageUrl: faker.image.url(),
-      sellerName: faker.person.fullName(),
-      ownerId: faker.string.uuid(),
+      sellerName: user.name,
     },
   });
-  return { categoryNameSkin };
+
+  return { categoryNameSkin, user, categoryItem };
 };
