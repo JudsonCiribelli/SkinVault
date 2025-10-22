d9eeb84cf0c0cffe4818b07cf009a8ab06a7e836
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-10-21 21:31:31 -0300

    feat: update create purchase service erros

--- src/services/order/createPurchaseService.ts
+++ src/services/order/createPurchaseService.ts
@@ -9,30 +9,28 @@ interface CreatePurchaseRequest {
 class CreatePurchaseService {
   private createCheckoutService: CreateCheckoutService;
 
-  // Usando injeção de dependência como discutimos
   constructor(createCheckoutService: CreateCheckoutService) {
     this.createCheckoutService = createCheckoutService;
   }
 
   async execute({ sellingItemId, buyerId }: CreatePurchaseRequest) {
-    // 1. VALIDAÇÕES (Seu código aqui estava perfeito)
     const sellingItem = await prismaClient.sellingItem.findUnique({
       where: { id: sellingItemId },
       include: { skin: true, order: true },
     });
 
     if (sellingItem!.userId === buyerId) {
-      throw new Error("Você não pode comprar seu próprio item.");
+      throw new Error("You cannot purchase your own item.");
     }
 
     const buyer = await prismaClient.user.findUnique({
       where: { id: buyerId },
     });
+
     if (!buyer) {
-      throw new Error("Comprador não encontrado.");
+      throw new Error("Buyer not found!");
     }
 
-    // 2. CRIA A ORDEM COM STATUS PENDENTE
     const newOrder = await prismaClient.order.create({
       data: {
         sellingItemId: sellingItemId,
@@ -43,7 +41,6 @@ class CreatePurchaseService {
       },
     });
 
-    // 3. CHAMA O SERVIÇO DE CHECKOUT PARA GERAR O LINK DE PAGAMENTO
     const checkoutData = await this.createCheckoutService.execute({
       orderId: newOrder.id,
       sellingItemId: sellingItem!.id,
@@ -53,7 +50,6 @@ class CreatePurchaseService {
       backUrl: "http://localhost:3000/minha-conta/compras",
     });
 
-    // 4. RETORNA OS DADOS DO CHECKOUT (INCLUINDO A URL)
     return { checkoutData };
   }
 }
