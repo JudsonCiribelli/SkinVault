f842457a5cb0e38be63cc419f3118a563c1a831d
Judson Ciribelli <judson.ciribelli17@gmail.com>
2025-11-07 10:33:54 -0300

    feat: add try catch to get user controller

--- src/controllers/user/getUserController.ts
+++ src/controllers/user/getUserController.ts
@@ -3,10 +3,17 @@ import { GetUserService } from "../../services/user/getUserService.ts";
 
 class GetUserController {
   async handle(req: Request, res: Response) {
-    const getUserService = new GetUserService();
-    const user = await getUserService.execute();
+    const userId = req.userId;
 
-    return res.status(200).send({ user });
+    try {
+      const getUserService = new GetUserService();
+      const user = await getUserService.execute({ userId });
+
+      return res.status(200).send({ user });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).send(error);
+    }
   }
 }
 export { GetUserController };
